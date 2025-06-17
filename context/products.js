020ffe0b8a2a057be82925f071d425af2f1acc4d
ShangYiQian <syq5678@gmail.com>
2025-06-18 03:45:54 +0800

    added more products and put in separate file

---
+++ context/products.js
@@ -0,0 +1,183 @@
+module.exports = [
+  {
+    id: "1",
+    name: "Cotton Shirt",
+    price: "$24.99",
+    image:
+      "https://images.unsplash.com/photo-1617137968427-85924c800a22?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
+    fabricComposition: ["Organic Cotton", "Polyester"],
+    ethicalCauses: ["Fair Labor"],
+    dealBreakers: [],
+  },
+  {
+    id: "2",
+    name: "Jeans",
+    price: "$39.99",
+    image:
+      "https://images.unsplash.com/photo-1624378439575-d8705ad7ae80?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
+    fabricComposition: ["Conventional Cotton"],
+    ethicalCauses: ["Recycled Materials"],
+    dealBreakers: [],
+  },
+  {
+    id: "3",
+    name: "Leather Jacket",
+    price: "$99.99",
+    image:
+      "https://images.unsplash.com/photo-1551028719-00167b16eac5?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
+    fabricComposition: ["Leather"],
+    ethicalCauses: [],
+    dealBreakers: ["Animal Products"],
+  },
+  {
+    id: "4",
+    name: "Polyester Tshirt",
+    price: "$9.99",
+    image:
+      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
+    fabricComposition: ["Polyester"],
+    ethicalCauses: [],
+    dealBreakers: ["Synthetic Virgin Polyester"],
+  },
+  {
+    id: "5",
+    name: "Silk Blouse",
+    price: "$49.99",
+    image:
+      "https://images.unsplash.com/photo-1581044777550-4cfa60707c03?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
+    fabricComposition: ["Silk"],
+    ethicalCauses: ["Women Empowerment"],
+    dealBreakers: [],
+  },
+  {
+    id: "6",
+    name: "Wool Coat",
+    price: "$149.99",
+    image:
+      "https://images.unsplash.com/photo-1554412933-514a83d2f3c8?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
+    fabricComposition: ["Wool"],
+    ethicalCauses: [],
+    dealBreakers: ["Animal Products"],
+  },
+  {
+    id: "7",
+    name: "Denim shirt",
+    price: "$34.99",
+    image:
+      "https://images.unsplash.com/photo-1589310243389-96a5483213a8?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80",
+    fabricComposition: ["Conventional Cotton", "Recycled Polyester"],
+    ethicalCauses: ["Fair Labor", "Eco-Friendly Dye"],
+    dealBreakers: [],
+  },
+  {
+    id: "8",
+    name: "Denim jeans",
+    price: "$29.99",
+    image:
+      "",
+    fabricComposition: ["Conventional Cotton"],
+    ethicalCauses: ["Women Empowerment", "Eco-Friendly Dye"],
+    dealBreakers: ["Animal Products"],
+  },
+  {
+    id: "9",
+    name: "Winter Coat",
+    price: "$174.99",
+    image:
+      "",
+    fabricComposition: ["Organic Cotton", "Wool"],
+    ethicalCauses: ["Fair Labor", "Recycled Materials"],
+    dealBreakers: ["Animal Products"],
+  },
+  {
+    id: "10",
+    name: "Graphic Tshirt",
+    price: "$14.99",
+    image:
+      "",
+    fabricComposition: ["Organic Cotton", "Recycled Polyester"],
+    ethicalCauses: ["Women Empowerment", "Eco-Friendly Dye"],
+    dealBreakers: [],
+  },
+  {
+    id: "11",
+    name: "Blouse",
+    price: "$34.99",
+    image:
+      "",
+    fabricComposition: ["Conventional Cotton"],
+    ethicalCauses: ["Recycled Materials", "Eco-Friendly Dye"],
+    dealBreakers: [],
+  },
+  {
+    id: "12",
+    name: "Jacket",
+    price: "$40.99",
+    image:
+      "",
+    fabricComposition: ["Polyester"],
+    ethicalCauses: ["Fair Labor"],
+    dealBreakers: ["Synthetic Virgin Polyester"],
+  },
+  {
+    id: "13",
+    name: "Denim shorts",
+    price: "$30.99",
+    image:
+      "",
+    fabricComposition: ["Organic Cotton", "Recycled Polyester"],
+    ethicalCauses: ["Women Empowerment", "Eco-Friendly Dye"],
+    dealBreakers: [],
+  },
+  {
+    id: "14",
+    name: "Long pants",
+    price: "$37.99",
+    image:
+      "",
+    fabricComposition: ["Linen"],
+    ethicalCauses: ["Fair Labor"],
+    dealBreakers: [],
+  },
+  {
+    id: "15",
+    name: "Black pants",
+    price: "$25.99",
+    image:
+      "",
+    fabricComposition: ["Leather"],
+    ethicalCauses: [],
+    dealBreakers: ["Animal Products"],
+  },
+  {
+    id: "16",
+    name: "Beige Dress",
+    price: "$34.99",
+    image:
+      "",
+    fabricComposition: ["Hemp", "Linen"],
+    ethicalCauses: ["Women Empowerment", "Recycled Materials"],
+    dealBreakers: [],
+  },
+  {
+    id: "17",
+    name: "Black shorts",
+    price: "$19.99",
+    image:
+      "",
+    fabricComposition: ["Polyester"],
+    ethicalCauses: [],
+    dealBreakers: ["Synthetic Virgin Polyester"],
+  },
+  {
+    id: "18",
+    name: "Dress",
+    price: "$24.99",
+    image:
+      "",
+    fabricComposition: ["Conventional Cotton"],
+    ethicalCauses: [],
+    dealBreakers: ["Non-transparent Sourcing"],
+  },
+
+];
